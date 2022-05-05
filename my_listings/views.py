0de7c60aad68f622aae7b7efe29410b9d9664a3b
Daniel Orn <danieloa21@ru.is>
2022-05-05 19:56:24 +0000

    sending data in my listings exampe

--- my_listings/views.py
+++ my_listings/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+mylist = [
+    {'Name': 'mikeay', 'price': 4.99},
+    {'Name': 'dadi', 'price': 50.99},
+
+]
+
+
 def index(request):
-    return render(request, 'my_listings/index.html')
+    return render(request, 'my_listings/ml_index.html', context={'mylist': mylist})
