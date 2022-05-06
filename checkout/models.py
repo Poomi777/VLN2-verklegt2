94294797d1a23d937910ac07b8cf7ae2a2c34acd
Daniel Orn <danieloa21@ru.is>
2022-05-06 10:30:32 +0000

    commenta out models

--- checkout/models.py
+++ checkout/models.py
@@ -1,30 +1,30 @@
 from django.db import models
-from userprofile.models import User
+#from userprofile.models import User
 
 # Create your models here.
 
 
-class Cart(models.Model):
-    cart_id = models.BigAutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    first_name = models.CharField(max_length=30, null=False, blank=False)
-    last_name = models.CharField(max_length=30, null=False, blank=False)
-    street_name = models.CharField(max_length=30, null=False, blank=False)
-    apartment_number = models.CharField(max_length=30, null=False, blank=False)
-    city = models.CharField(max_length=30, null=False, blank=False)
-    country = models.CharField(max_length=30, null=False, blank=False)
-    postal_code = models.IntegerField()
+#class Cart(models.Model):
+#    cart_id = models.BigAutoField(primary_key=True)
+#    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+#    first_name = models.CharField(max_length=30, null=False, blank=False)
+#    last_name = models.CharField(max_length=30, null=False, blank=False)
+#    street_name = models.CharField(max_length=30, null=False, blank=False)
+#    apartment_number = models.CharField(max_length=30, null=False, blank=False)
+#    city = models.CharField(max_length=30, null=False, blank=False)
+#    country = models.CharField(max_length=30, null=False, blank=False)
+#    postal_code = models.IntegerField()
 
-    def __str__(self):
-        return self.name
+#    def __str__(self):
+#        return self.name
 
 
-class Payment(models.Model):
-    cart_id = models.ForeignKey(Cart, on_delete=models.SET_NULL, null=True)
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    cardholder_name = models.CharField(max_length=30, null=False, blank=False)
-    card_number = models.IntegerField()
-    month_expiration = models.IntegerField()
-    year_expiration = models.IntegerField()
-    cvc = models.IntegerField()
+#class Payment(models.Model):
+#    cart_id = models.ForeignKey(Cart, on_delete=models.SET_NULL, null=True)
+#    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+#    cardholder_name = models.CharField(max_length=30, null=False, blank=False)
+#    card_number = models.IntegerField()
+#    month_expiration = models.IntegerField()
+#    year_expiration = models.IntegerField()
+#    cvc = models.IntegerField()
 
