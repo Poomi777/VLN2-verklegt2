48783b22da54787902a834a1b8a5db09f95204bd
Poomi <pkm.poomi@gmail.com>
2022-05-08 22:26:51 +0000

    date field added to models in my_listings again

--- my_listings/models.py
+++ my_listings/models.py
@@ -16,7 +16,7 @@ class Listing(models.Model):
     listing_price = models.FloatField()
     listing_highest_offer = models.CharField(max_length=30)
     listing_image_url = models.CharField(max_length=500, default="https://d25tv1xepz39hi.cloudfront.net/2016-07-16/files/cat-sample_1313.jpg")
-    listing_date = models.DateTimeField()
+    listing_date = models.DateTimeField(auto_now_add=0)
 
     def __str__(self):
        return self.link
