766695a6549666bbac26570478b4527ee773a822
Poomi <pkm.poomi@gmail.com>
2022-05-08 21:54:46 +0000

    models for checkout, my_bids, new_listing and view_listing updated

--- my_bids/models.py
+++ my_bids/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from my_listings.models import Listing
+from userprofile.models import User
 
 # Create your models here.
+
+
+class Bids(models.Model):
+    user_id = models.ForeignKey(User)
+    product_id = models.ForeignKey(Listing)
+    bid_price = models.FloatField()
+    bid_accepted = models.NullBooleanField()
+
+    def __str__(self):
+        return self.bid_price
--- new_listing/models.py
+++ new_listing/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from my_listings.models import Listing
 
 # Create your models here.
