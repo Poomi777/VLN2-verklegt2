b137a201668e4e6ae9ebd8c86a2b2632f27f6068
Poomi <pkm.poomi@gmail.com>
2022-05-09 20:50:07 +0000

    Lots of files changed, in html, in css and models. But we are now able to show items from the database

--- my_listings/views.py
+++ my_listings/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from my_listings.models import Listing
 
 # Create your views here.
-mylist = [
-    {'Name': 'mikeay', 'price': 4.99},
-    {'Name': 'dadi', 'price': 50.99},
+#mylist = [
+#    {'Name': 'mikeay', 'price': 4.99},
+#    {'Name': 'dadi', 'price': 50.99},
 
-]
+#]
 
 
 def index(request):
-    return render(request, 'my_listings/ml_index.html', context={'mylist': mylist})
+    context = {'listings': Listing.objects.all().order_by('listing_date')}
+    return render(request, 'my_listings/ml_index.html', context)
