a5a0db11a78dcc3a3e3012baf912e3ee5d45826c
Poomi <pkm.poomi@gmail.com>
2022-05-10 12:17:50 +0000

    added image to userprofile app model

--- userprofile/models.py
+++ userprofile/models.py
@@ -11,6 +11,7 @@ class User(models.Model):
     address = models.CharField(max_length=100, null=False, blank=False)
     country = models.CharField(max_length=100, null=False, blank=False)
     rating = models.IntegerField()
+    image = models.CharField(max_length=500, default="https://d25tv1xepz39hi.cloudfront.net/2016-07-16/files/cat-sample_1313.jpg")
 
     def __str__(self):
         return self.name
