e2a79abf2a27dade19bd6475900b56fa1011fc59
Daniel Orn <danieloa21@ru.is>
2022-05-10 18:13:07 +0000

    homepage styleing start

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/homepage
     path('', views.index, name="homepage-index"),
+    path('<int:id>', views.get_listing_by_id, name="listing_details")
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from my_listings.models import Listing
 # Create your views here.
 from templates import homepage
@@ -7,6 +7,11 @@ from templates import homepage
 def index(request):
     context = {'Listings': Listing.objects.all().order_by('name')}
     return render(request, 'homepage/index.html', context)
+
+def get_listing_by_id(request, id):
+    return render(request, 'homepage/listing_details.html', {
+        'listing': get_object_or_404(Listing, pk=id)
+    })
 
 """def all_listings_price(request):
     context = {'Listings': Listing.objects.all().order_by('name')}
