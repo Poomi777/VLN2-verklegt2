50af6a72448a68a53f3439c00954ceab6adb3afb
Daniel Orn <danieloa21@ru.is>
2022-05-11 19:47:37 +0000

    auth

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     # http://localhost:8000/profile
     path('', views.index, name="profile-index"),
+    path('register', views.register, name='register'),
+    path('login', LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout')
 
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 """my_profile = [
@@ -6,6 +7,16 @@ from django.shortcuts import render
 ]"""
 def index(request):
     return render(request, 'userprofile/profile_index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(date=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(request, 'userprofile/register.html', {
+        'form': UserCreationForm()
+    })
 
 """def myprofile(request):
     return render(request, 'navigation.html', context={'my_profile': myprofile})"""
