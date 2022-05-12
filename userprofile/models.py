1e51aad049cb1da43c4c7164107b66e7869acb7f
Poomi <pkm.poomi@gmail.com>
2022-05-12 23:02:13 +0000

    migrations finally working?

--- userprofile/models.py
+++ userprofile/models.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 
 class Userinfo(models.Model):
     userinfo_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=100, null=False, blank=False)
-    country = models.CharField(max_length=100, null=False, blank=False)
+    address = models.CharField(max_length=100, null=True)
+    country = models.CharField(max_length=100, null=True)
     rating = models.IntegerField()
     image = models.CharField(max_length=500, default="https://d25tv1xepz39hi.cloudfront.net/2016-07-16/files/cat-sample_1313.jpg%22")
 
