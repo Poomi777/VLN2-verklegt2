9f2c06be067814d96f37147a0c5141f1e3f1ffb2
Poomi <pkm.poomi@gmail.com>
2022-05-13 23:30:48 +0000

    checkout done

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     # http://localhost:8000/checkout
-    path('', views.index, name="checkout-index"),
-    path('payment', views.payment, name="payment-index"),
-    path('review_order', views.review_order, name="review_order-index"),
-    path('order_confirmed', views.order_confirmed, name="order_confirmed-index"),
+    path('<int:id>', views.index, name="checkout-index"),
+    path('payment/<int:id>', views.payment, name="payment-index"),
+    path('review_order/<int:id>', views.review_order, name="review_order-index"),
+    path('order_confirmed/<int:id>', views.order_confirmed, name="order_confirmed-index"),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -2,23 +2,27 @@ from django.shortcuts import render, get_object_or_404, redirect
 from checkout.forms.checkout_form import ShippingCreateForm
 from checkout.forms.payment_form import PaymentCreateForm
 from checkout.models import Cart
+from my_listings.models import Listing
 
 # Create your views here.
 from userprofile.models import Userinfo
 
 
-def index(request): #Technically def create_shipping_information
+def index(request, id): #Technically def create_shipping_information
     if request.method == 'POST':
         form = ShippingCreateForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('payment-index')
+            return redirect('payment-index', {
+                'listing': get_object_or_404(Listing, pk=id)
+            })
     else:
         form = ShippingCreateForm()
     return render(request, 'checkout/checkoutindex.html', {
-        'form': form})
+        'form': form,
+        'listing': get_object_or_404(Listing, pk=id)})
 
-def payment(request):
+def payment(request, id):
     userinfovar = Userinfo.objects.filter(userinfo_id=request.user).first()
 
     if request.method == 'POST':
@@ -30,19 +34,22 @@ def payment(request):
         form = PaymentCreateForm()
     return render(request, 'checkout/payment.html', {
         'form': form,
-        'userprofile': get_object_or_404(Userinfo, userinfo_id=userinfovar.userinfo_id)
+        'userprofile': get_object_or_404(Userinfo, userinfo_id=userinfovar.userinfo_id),
+        'listing': get_object_or_404(Listing, pk=id)
     })
 
-def review_order(request):
+def review_order(request, id):
     userinfovar = Userinfo.objects.filter(userinfo_id=request.user).first()
 
     return render(request, 'checkout/review_order.html', {
-        'userprofile': get_object_or_404(Userinfo, userinfo_id=userinfovar.userinfo_id)
+        'userprofile': get_object_or_404(Userinfo, userinfo_id=userinfovar.userinfo_id),
+        'listing': get_object_or_404(Listing, pk=id)
     })
 
-def order_confirmed(request):
+def order_confirmed(request, id):
     userinfovar = Userinfo.objects.filter(userinfo_id=request.user).first()
 
     return render(request, 'checkout/order_confirmed.html', {
-        'userprofile': get_object_or_404(Userinfo, userinfo_id=userinfovar.userinfo_id)
+        'userprofile': get_object_or_404(Userinfo, userinfo_id=userinfovar.userinfo_id),
+        'listing': get_object_or_404(Listing, pk=id)
     })
