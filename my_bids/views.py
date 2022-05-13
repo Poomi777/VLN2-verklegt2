393cdc487cced057041d301ef44171197816d9b6
Daniel Orn <danieloa21@ru.is>
2022-05-13 23:30:21 +0000

    bids

--- my_bids/views.py
+++ my_bids/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 
+from my_bids.models import Bids
+from userprofile.models import Userinfo
+from my_listings.models import Listing
 
 # Create your views here.
-from userprofile.models import Userinfo
+
 
 
 def index(request):
     userinfovar = Userinfo.objects.filter(userinfo_id=request.user).first()
+    user = request.user
+    ids = list()
+    for i in Bids.objects.all():
+        if i.buyer_id == user:
+            ids.append(i.listing_id_id)
+    mybids = Bids.objects.filter(buyer_id=user)
+    listings = Listing.objects.filter(listing_id__in=ids)
     return render(request, 'my_bids/mbindex.html', {
-        'userprofile': get_object_or_404(Userinfo, userinfo_id=userinfovar.userinfo_id)
+        'userprofile': get_object_or_404(Userinfo, userinfo_id=userinfovar.userinfo_id),
+        'listings': listings,
+        'mybids': mybids
     })
+
