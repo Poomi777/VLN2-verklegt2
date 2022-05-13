21e7af077b1412d79a11d52b16f612eac01afee3
Poomi <pkm.poomi@gmail.com>
2022-05-13 23:55:29 +0000

    checkout

--- my_bids/urls.py
+++ my_bids/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+
+import checkout
+from checkout.views import index
 from . import views
 
 urlpatterns = [
     # http://localhost:8000/my_bids
-    path('', views.index, name="my_bids-index")
+    path('', views.index, name="my_bids-index"),
+    path(r'^<int:id>$', checkout.views.index, name="checkout-index")
     ]
