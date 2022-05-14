bf356fa096d0fb04d113c785c5789d5dda7e9ac1
Poomi <pkm.poomi@gmail.com>
2022-05-14 13:55:23 +0000

    login redirect fixed

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # http://localhost:8000/profile
     #path('', views.index, name="profile-index"),
     path('register', views.register, name='register'),
-    path('login', LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('login', LoginView.as_view(next_page='profile', template_name='userprofile/login.html'), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('profile', views.userinforegister, name='profile'),
     path('user_pic', views.user_pic, name='user_pic')
